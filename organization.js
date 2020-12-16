76a62f2abcfe17db45b404f318d651afd97f5fff
Thomas Dawson <dawsontc003@gmail.com>
2020-12-16 00:27:52 -0700

    updated all npm packages

--- organization.js
+++ organization.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const cTable = require("console.table");
+const logo = require("asciiart-logo");
+const config = require("./package.json");
+console.log(logo(config).render());
 
 const connection = mysql.createConnection({
   host: "localhost",
