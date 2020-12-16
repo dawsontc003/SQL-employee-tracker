f6c30b9341ff53a91c9ee652026174a8caca4da9
Thomas Dawson <dawsontc003@gmail.com>
2020-12-16 00:13:00 -0700

    Initial app actions with switch statement for each option

--- organization.js
+++ organization.js
@@ -1,3 +1,4 @@
+const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
@@ -11,4 +12,67 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log(`Connect at ${connection.threadId}`);
+  menu();
 });
+
+// Function get request from user
+const menu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "what would you like to do?",
+        name: "action",
+        choices: [
+          "View Employees",
+          "View Departments",
+          "View Roles",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then(({ action }) => {
+      switch (action) {
+        case "View Employees":
+          console.log("employee DB call");
+          break;
+        case "View Departments":
+          console.log("department DB call");
+          break;
+        case "View Roles":
+          console.log("Role DB call");
+          break;
+        case "Add Department":
+          console.log("add department call");
+          break;
+        case "Add Role":
+          console.log("add role call");
+          break;
+        case "Add Employee":
+          console.log("add employee call");
+          break;
+        case "Update Employee Role":
+          console.log("update employee call");
+          break;
+        case "Exit":
+          console.log("Exit app");
+          connection.end();
+
+        // code block
+      }
+    });
+};
+
+// const menu = () => {
+//   connection.query("SELECT name FROM colleges", (err, res) => {
+//     if (err) throw err;
+
+//     // Log all results of the SELECT statement
+//     console.log(res);
+//     connection.end();
+//   });
+// };
