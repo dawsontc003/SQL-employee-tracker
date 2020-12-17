b3fafce6838e17e4cc14dbdc612d1472c17d4326
Thomas Dawson <dawsontc003@gmail.com>
2020-12-17 16:07:52 -0700

    updated sql table with correct foreign keys

--- org_db_seeddata.sql
+++ org_db_seeddata.sql
@@ -15,8 +15,9 @@ CREATE TABLE role (
   title VARCHAR(30) NULL,
   salary DECIMAL(8,2) NULL,
   department_id INT,
-  PRIMARY KEY (id)
-);
+  PRIMARY KEY (id),
+    FOREIGN KEY (department_id) references department(id)
+  );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
@@ -24,15 +25,17 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+   FOREIGN KEY (role_id) references role(id),
+   FOREIGN KEY (manager_id) references employee(id)
 );
 
 
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("production");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("director", 100000.00, 12);
+VALUES ("director", 100000.00, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Thomas", "Dawson", 1, null);
